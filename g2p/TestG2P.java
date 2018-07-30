e32af364e23499cd9d54262b2ca915759de0fbf5
renancunha <renancunhafonseca@hotmail.com>
2018-07-30 10:32:26 -0300

    add options library

--- g2p/TestG2P.java
+++ g2p/TestG2P.java
@@ -1,11 +1,12 @@
 package g2p;
+import org.apache.commons.cli.*;
 
 /* importa conteúdo da biblioteca g2plib */
 import falabrasil.g2p.GraphemeToPhoneme;
 
 public class TestG2P {
 	public static void main(String[] args) {
-		if(args.length != 1) {
+		if(args.length < 1) {
 			System.err.println("Grupo FalaBrasil");
 			System.out.println("Conversor Grafema-Fonema (G2P)\n");
 			System.err.println("Usage: java -cp \".:g2p/g2plib.jar\" " +
@@ -15,10 +16,25 @@ public class TestG2P {
 			System.exit(1);
 		}
 
+		Options options = new Options();
+		options.addOption("f",true,"file");
+		options.addOption("w",true,"word");
+
+		CommandLineParser parser = new DefaultParser();
+
+		try{
+			CommandLine cmd = parser.parse(options, args);
+		
+			GraphemeToPhoneme phones = new GraphemeToPhoneme();
+			if(cmd.hasOption("w")){
+				System.out.println(args[1] + "\t" + phones.g2p(args[1]));
+			}
+
 		/* instancia objeto para usar os métodos da biblioteca g2plib */
-		GraphemeToPhoneme phones = new GraphemeToPhoneme();
 
 		/* escreve fonemas da primeira palavra passada como parâmetro na tela usando método g2p() */
-		System.out.println(args[0] + "\t" + phones.g2p(args[0]));
+		} catch(Exception e){
+			System.out.println(e.getMessage());
+		}
 	}
 }
